ae3aba652ddb7f0317266507eae1d27b92e34582
amiya-ps-559 <amiya.rout@publicissapient.com>
2025-12-05 14:44:49 +0530

    error-testing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_project",
-    version="0.0.3",
+    version="0.0.4",
     description="This contains the code in the ./src directory of the project",
     author="Amiya Rout",
     packages=find_packages(where="./src"),
